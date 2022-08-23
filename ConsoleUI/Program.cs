5e049b6873c1862aed623b602cadaca7f0147feb
Ефимов Никита Сергеевич <efimov@topsystems.ru>
2022-08-23 08:30:21 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleUI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
